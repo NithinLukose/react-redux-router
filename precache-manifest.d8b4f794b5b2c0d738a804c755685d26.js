cbd7d78c02db1457fe87d516d24122d7661c0e61
NithinLukose <nithinclukose12@gmail.com>
2020-06-28 11:53:21 +0530

    Updates

---
+++ precache-manifest.d8b4f794b5b2c0d738a804c755685d26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02902f9f30126f6002c634a27f11b898",
+    "url": "/post-app/index.html"
+  },
+  {
+    "revision": "923c231fa71eab290980",
+    "url": "/post-app/static/css/main.f67eaba8.chunk.css"
+  },
+  {
+    "revision": "06671169d66f4e40e768",
+    "url": "/post-app/static/js/2.7bb2cf56.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/post-app/static/js/2.7bb2cf56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923c231fa71eab290980",
+    "url": "/post-app/static/js/main.5471c468.chunk.js"
+  },
+  {
+    "revision": "22901732feb1a8979a07",
+    "url": "/post-app/static/js/runtime-main.147ba7cf.js"
+  }
+]);
